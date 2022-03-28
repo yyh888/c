0f8dbb87f22750407a663acae9b044325620adbe
yyh888 <1161752748@qq.com>
2022-03-28 14:30:37 +0800

    联合体

---
+++ test_3_28/test_3_28/test.c
@@ -0,0 +1,74 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
+//struct S
+//{
+//	int a : 2;
+//	int b : 10;
+//	int c : 30;
+//	int d : 32;
+//};
+//
+//
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));
+//	return 0;
+//}
+
+
+//struct S
+//{
+//	char a : 3;
+//	char b : 4;
+//	char c : 5;
+//	char d : 4;
+//};
+//
+//
+//int main()
+//{
+//	struct S s = { 0 };
+//	s.a = 10;
+//	s.b = 20;
+//	s.c = 3;
+//	s.d = 4;
+//	return 0;
+//}
+
+
+//enum coler
+//{
+//	red,
+//	yellow,
+//	green
+//};
+//
+//
+//int main()
+//{
+//	printf("%d %d %d\n", red,yellow,green);
+//	return 0;
+//}
+
+//union Un
+//{
+//	int i;
+//	char c;
+//};
+//union Un u;
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(u));
+//	printf("%p\n", &(u.i));
+//	printf("%p\n", &(u.c));
+//	printf("%p\n", &u);
+//	return 0;
+//}
+
+
+
