7b8eb11eb707dcb671de7c628643e1cdc6dea5a7
yyh888 <1161752748@qq.com>
2022-03-30 22:06:20 +0800

    动态内存

---
+++ test_3_30/test_3_30/test.c
@@ -0,0 +1,78 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+#include<stdlib.h>
+#include<string.h>
+
+//int main()
+//{
+//	int* p = (int*)malloc(INT_MAX);
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 5; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 5; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int* p = (int*)malloc(20);
+//	if (p == NULL)
+//	{
+//		printf("%s", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 5; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//	}
+//	int* ptr = (int*)realloc(p, 40);
+//	if (ptr != NULL)
+//	{
+//		p = ptr;
+//		int i = 0;
+//		for (i = 5; i < 10; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	free(p);
+//		p = NULL;
+//	return 0;
+//}
+
