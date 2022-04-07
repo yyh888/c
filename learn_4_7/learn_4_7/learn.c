498ac5f1970fe0f22a3df86ef9a19722b825eda7
yyh888 <1161752748@qq.com>
2022-04-07 23:29:38 +0800

    C深刨

---
+++ learn_4_7/learn_4_7/learn.c
@@ -0,0 +1,104 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<stdbool.h>
+#include<math.h>
+#include<float.h>
+//int main()
+//
+//{
+//	int a = 0xAABBCCDD;
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned int a = -10;
+//	printf("%d\n", a);
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	unsigned int i = 0;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
+
+
+//int Isempty()
+//
+//{
+//	return 1;
+//}
+//
+//
+//int main()
+//{
+//	if (Isempty())
+//	{
+//		printf("yes\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("1 ") && printf("0 ");
+//	printf("1 ") || printf("0 ");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	bool x = true;
+//	x = false;
+//	printf("%zd\n", sizeof(x));
+//	return 0;
+//}
+//int main()
+//{
+//	BOOL x = TRUE;
+//	x = FALSE;
+//	printf("%zd\n", sizeof(x));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	double d = 3.6;
+//	printf("%.50f", 3.6);
+//	return 0;
+//}
+
+
+
+//#define EPS 0.0000000001
+
+//int main()
+//{
+//	double x = 1.0;
+//	double y = 0.1;
+//	printf("%.50f\n", x - 0.9);
+//	printf("%.50f\n", y);
+//	if (fabs((x - 0.9) - y) < DBL_EPSILON)
+//	{
+//		printf("you can see me\n"); 
+//	}
+//	else 
+//	{
+//		printf("opps\n");
+//	}
+//	return 0;
+//}
+
+
