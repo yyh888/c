ceb2c93b37f65e873047dc54a95d173fc48097b9
yyh888 <1161752748@qq.com>
2022-04-11 23:17:42 +0800

    C深刨

---
+++ learn_4_11/learn_4_11/learn.c
@@ -0,0 +1,88 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<Windows.h>
+
+//int main()
+//{
+//	printf("hello world\n");
+//#if 1
+//	printf("hello world\n");
+//	printf("hello world\n");
+//	printf("hello world\n");
+//	printf("hello world\n");
+//#endif
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//	if (1 == a && \
+//		2 == b && \
+//		3 == c)
+//	{
+//		printf("yes\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	const char* p = "|/-\\";
+//	while (1)
+//	{
+//		i %= 4;
+//		printf("[%c]\r", p[i]);
+//		i++;
+//		Sleep(30);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 10;
+//	for (; i > 0; i--)
+//	{
+//		printf("%2d\r", i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof('a'));
+//	printf("%d\n", sizeof(""));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char c = 'abcd';
+//	printf("%c\n", c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (; i < 128; i++)
+//	{
+//		printf("%c\n", i);
+//		Sleep(500);
+//	}
+//	return 0;
+//}
+
+
+
