c37f1b161a5db2b97820a9bcd4afd73f57c1e0ba
yyh888 <1161752748@qq.com>
2022-04-17 21:29:46 +0800

    猜数字，关机

---
+++ learn_4_17/learn_4_17/learn.c
@@ -0,0 +1,82 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<string.h>
+//int main()
+//{
+//	char ch1[] = "welcome to bit!!!";
+//	char ch2[] = "#################";
+//	int sz = strlen(ch1);
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		ch2[left] = ch1[left];
+//		ch2[right] = ch1[right];
+//		printf("%s\n", ch2);
+//		Sleep(1000);
+//		left++;
+//		right--;
+//		system("cls");
+//	}
+//	printf("%s\n", ch2);
+//	return 0;
+//}
+
+//void cmp(int* x, int* y)
+//{
+//	int c = *x;
+//	*x = *y;
+//	*y = c;
+//}
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	cmp(&a, &b);
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int i = 0;
+//		int j = 0;
+//		for (i = 1; i <= n; i++)
+//		{
+//			for (j = 1; j <= i; j++)
+//			{
+//				printf("%2d*%d=%3d ", i, j, i * j);
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char input[10] = { 0 };
+//	system("shutdown -s -t 60");
+//	while (1)
+//	{
+//		printf("你的电脑将在60s后关机，输入：我是猪，就停止\n");
+//		scanf("%s", input);
+//		if (strcmp(input, "我是猪") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+
